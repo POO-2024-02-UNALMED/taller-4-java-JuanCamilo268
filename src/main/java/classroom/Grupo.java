e69a9bc25dcc6c361945e474bcc9768dfd36d82a
JuanCamilo268 <juleonba@unal.edu.co>
2024-11-19 20:26:09 -0500

    Update Grupo.java

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,7 +5,7 @@ public class Grupo {
     public Persona[] estudiantes;
     public Persona profesor;
     public Asignatura asignatura;
-    public final int codigo = 0;
+    public final int codigo; ///= 0; lo quito para que la constante no inicie en 0;
     public String horario;
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
@@ -17,14 +17,20 @@ public class Grupo {
     }
 
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        Persona[] personas = new Persona[cantidadEstudiantes];
-        this(personas, profesor, asignatura, codigo, horario);
+        Persona[] personas  = new Persona[cantidadEstudiantes];
+        ///this(personas, profesor, asignatura, codigo, horario); lo reemplazo porque esta línea debe ser la primera del constructor, y eso no es compatible con la forma de designar estudantes
+        this.estudiantes = personas;
+        this.profesor = profesor;
+        this.asignatura = asignatura;
+        this.codigo = codigo;
+        this.horario = horario;
     }
 
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
+        this.codigo = 0; ///Lo pongo poruqe una constante debe ser inicializada de cualquier forma
     }
 
     void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
